801a96d1e30223057286406b200c362630a1515a
coredelle <westga09@gmail.com>
2022-01-11 15:32:23 -0500

    add top text and buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Container from "react-bootstrap/Container";
-import { Stack } from "react-bootstrap";
+import { Button, Stack } from "react-bootstrap";
 
 function App() {
   return (
-    <Container>
+    <Container className='my-4'>
       <Stack direction='horizontal' gap='2' className='mb-4'>
-
+        <h1 className='me-auto'>Budgets</h1>
+        <Button variant='primary'>Add Budget</Button>
+        <Button variant='outline-primary'>Add Expense</Button>
       </Stack>
     </Container>
   );
