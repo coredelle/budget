fd5651253e121a0d15dd42f1ebb722e179a8f7fb
coredelle <westga09@gmail.com>
2022-03-09 11:51:19 -0500

    update

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import BudgetCard from "./components/BudgetCard";
 
 function App() {
   return (
+    // my gives mergin at top and bottom of page
     <Container className='my-4'>
+        {/*gap is the space between elements, mb is margin bottom*/}
       <Stack direction='horizontal' gap='2' className='mb-4'>
+        {/*  me sticks it to the left side of the screen*/}
         <h1 className='me-auto'>Budgets</h1>
+        {/*  variant gives color*/}
         <Button variant='primary'>Add Budget</Button>
         <Button variant='outline-primary'>Add Expense</Button>
       </Stack>
@@ -16,7 +20,7 @@ function App() {
           gap: '1rem',
           alignItems: 'flex-start'
       }}>
-      <BudgetCard></BudgetCard>
+      <BudgetCard name={'Freak Nik'} amount={5000} max={105000}/>
       </div>
     </Container>
   );
