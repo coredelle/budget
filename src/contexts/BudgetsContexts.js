062f4b58e786c4703e69fb58052a05de8da59795
coredelle <westga09@gmail.com>
2022-03-09 14:31:11 -0500

    setup before using data

--- src/contexts/BudgetsContexts.js
+++ src/contexts/BudgetsContexts.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, {useContext, useState} from "react";
 
 // context helps pass info easily
 const BudgetsContext = React.createContext();
@@ -7,10 +7,51 @@ export function useBudgets() {
     return useContext(BudgetsContext);
 }
 
+// expense {
+//     id:
+//     name:
+//     max
+// }
+
+// budget {
+//     id:
+//     budgetId:
+//     amount:
+//     description:
+// }
+
+
 export const BudgetsProvider = ({ children }) => {
+    const [budgets, setBudgets] = useState([]);
+    const [expenses, setExpenses] = useState([]);
+
+    function getBudgetExpenses() {
+
+    }
+    function addExpense() {
+
+    }
+    function addBudget() {
+
+    }
+    function deleteExpense() {
+
+    }
+    function deleteBudget() {
+
+    }
+
     return (
         // this allows all children to have access to whatever the value is (empty object right now)
-        <BudgetsContext.Provider value={{}}>
+        <BudgetsContext.Provider value={{
+            budgets,
+            expenses,
+            getBudgetExpenses,
+            addExpense,
+            addBudget,
+            deleteExpense,
+            deleteBudget
+        }}>
             {children}
         </BudgetsContext.Provider>
     )
